73c7a67bdf6e6df2b253e452d659fce58a08d6b1
Eza Febrian <febrianezza@gmail.com>
2020-12-20 17:12:21 +0700

    add toast

--- app/src/main/java/com/ezafebrian/ci_test/MainActivity.kt
+++ app/src/main/java/com/ezafebrian/ci_test/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ezafebrian.ci_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        * Simple toast to test
+        * Continuous-integration*/
+        Toast.makeText(this, "Hello, World", Toast.LENGTH_SHORT).show()
     }
 }
